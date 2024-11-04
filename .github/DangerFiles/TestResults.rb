4e811568f5117c4bc355b3811003e855c51f61fb
Brandon Page <bpage@salesforce.com>
2024-11-03 20:43:44 -0800

    no message

--- .github/DangerFiles/TestResults.rb
+++ .github/DangerFiles/TestResults.rb
@@ -1,9 +1,3 @@
-require 'json'
-
 xcode_summary.ignores_warnings = true
 xcode_summary.inline_mode = true
 xcode_summary.report '../../test.xcresult'
-
-# Only print on PR if tests failed.
-test_failures = JSON.parse(xcode_summary.warning_error_count)['errors']
-xcode_summary.test_summary(test_failures > 0)
