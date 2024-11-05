81e955b002743cf3937570edffad98b440abdde2
Brandon Page <bpage@salesforce.com>
2024-11-04 19:15:19 -0800

    no message

--- .github/DangerFiles/TestResults.rb
+++ .github/DangerFiles/TestResults.rb
@@ -1,7 +1,7 @@
 xcode_summary.ignores_warnings = true
 xcode_summary.inline_mode = true
 
-if File.file?('../../test.xcresult')
+if File.exists?('../../test.xcresult')
     xcode_summary.report '../../test.xcresult'
 else
     fail "No test results found."
