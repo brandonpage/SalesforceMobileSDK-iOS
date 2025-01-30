6d3c31aca7e55fb4b3b20b4fba6d02375c2b6957
Brandon <37725848+brandon-page@users.noreply.github.com>
2025-01-29 21:07:06 -0800

    Update SFSDKAnalyticsManager.h
    touch analytics file

--- libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Manager/SFSDKAnalyticsManager.h
+++ libs/SalesforceAnalytics/SalesforceAnalytics/Classes/Manager/SFSDKAnalyticsManager.h
@@ -27,6 +27,7 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
 #import <SalesforceAnalytics/SFSDKEventStoreManager.h>
 
 @interface SFSDKAnalyticsManager : NSObject
