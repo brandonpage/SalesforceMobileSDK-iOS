f107e25731244b04acbb5d4a7d8ad4da21ed05b4
Brandon Page <bpage@salesforce.com>
2025-09-12 12:01:34 -0700

    Force all tests to run for testing purposes.

--- .github/DangerFiles/TestOrchestrator.rb
+++ .github/DangerFiles/TestOrchestrator.rb
@@ -4,18 +4,18 @@
 warn("Big PR, try to keep changes smaller if you can.", sticky: true) if git.lines_of_code > 1000
 
 # Redirect contributors to PR to dev.
-fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
+# fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
 
 # List of supported xcode schemes for testing
 SCHEMES = ['SalesforceSDKCommon', 'SalesforceAnalytics', 'SalesforceSDKCore', 'SmartStore', 'MobileSync']
 
-modifed_libs = Set[]
-for file in (git.modified_files + git.added_files);
-    scheme = file.split("libs/").last.split("/").first
-    if SCHEMES.include?(scheme) 
-        modifed_libs.add(scheme)
-    end
-end
+modifed_libs = SCHEMES
+# for file in (git.modified_files + git.added_files);
+#     scheme = file.split("libs/").last.split("/").first
+#     if SCHEMES.include?(scheme) 
+#         modifed_libs.add(scheme)
+#     end
+# end
 
 # Set Github Job output so we know which tests to run
 json_libs = modifed_libs.map { |l| "'#{l}'"}.join(", ")
